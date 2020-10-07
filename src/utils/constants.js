826fa291e8699526476f2a0d35cc93370baedb78
pankajvashisht-ucreate <pankaj@ucreate.co.in>
2020-10-07 10:21:58 +0530

    add some changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,21 +1,11 @@
 export const queryTypes = {
-  select: 'get',
-  update: 'updateOrCreate',
-  delete: 'destroy',
-  insert: 'create'
+	select: "get",
+	update: "updateOrCreate",
+	delete: "destroy",
+	insert: "create",
 };
 
 export const allConditions = {
-  and: 'where',
-  or: 'orWhere',
-  in: 'whereIn',
-  '>': 'where',
-  '<': 'where',
-  'between': 'whereBetween',
-  'not in': 'whereNotIn',
-  'null': 'whereNull',
-  'not null': 'whereNotNull',
-  'exists': 'whereExists',
-  'created_at': 'whereDate'
-
+	and: "where",
+	or: "orWhere",
 };
